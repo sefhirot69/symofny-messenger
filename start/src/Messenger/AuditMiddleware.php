b80c7fd87195ca4212c839620a183dc17db36b14
josealiaslloret <josalillo@gmail.com>
2022-06-26 12:57:19 +0200

    feat : register log with message process

--- start/src/Messenger/AuditMiddleware.php
+++ start/src/Messenger/AuditMiddleware.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace App\Messenger;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
+use Symfony\Component\Messenger\Stamp\ReceivedStamp;
+use Symfony\Component\Messenger\Stamp\SentStamp;
 
 final class AuditMiddleware implements MiddlewareInterface
 {
+
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $messengerAuditLogger)
+    {
+        $this->logger = $messengerAuditLogger;
+    }
 
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
@@ -20,8 +34,22 @@ final class AuditMiddleware implements MiddlewareInterface
         /** @var UniqueIdStamp $stamp */
         $stamp = $envelope->last(UniqueIdStamp::class);
 
-        dump($stamp->getUniqueid());
+        $context = [
+            'id' => $stamp->getUniqueid(),
+            'class' => get_class($envelope->getMessage())
+        ];
+
+        $envelope = $stack->next()->handle($envelope, $stack);
+
+        if ($envelope->last(ReceivedStamp::class)) {
+            $this->logger->info('[{id}] Received {class}', $context);
+        } elseif ($envelope->last(SentStamp::class)) {
+            $this->logger->info('[{id} Sent {class}]', $context);
+        } else {
+            $this->logger->info('[{id}] Handling sync {class}', $context);
+        }
+
         //Esto ejecuta el siguiente middleware y luego devuelve su valor
-        return $stack->next()->handle($envelope, $stack);
+        return $envelope;
     }
 }
